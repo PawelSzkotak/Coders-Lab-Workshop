2389db0f432d1232aad5b659d154760d2c993c4a
Paweł Szkotak <34739982+PawelSzkotak@users.noreply.github.com>
2018-03-22 14:17:05 +0100

    Sit On Chair - Work in progress
    Dodałem ukrywanie paska opisu po najechaniu na obrazek. W sumie za pomocą css :hover mozna to zrobić łatwiej ale tak dla odmiany zrobiłem w JS.
    Zmieniam kolory tabliczek z planami ofert - nie wiem czy tak miało byc czy tylko ta na środku miała na stałe mieć inny kolor.
    Po dodaniu kodu rozjechała mi sie totalnie sekcja kalkulatora.
    Do zrobienia - kalkulator, czcionka, ładniejsza organizacja kodu.

---
+++ js/bestSellerTag.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+  var bestSellerFigures = document.querySelectorAll('.bestsellers figure');
+  console.log(bestSellerFigures);
+
+  bestSellerFigures.forEach(function(figure) {
+    figure.firstElementChild.addEventListener('mouseover', function(){
+      this.parentElement.lastElementChild.style.visibility = 'hidden';
+    });
+
+    figure.firstElementChild.addEventListener('mouseout', function(){
+      this.parentElement.lastElementChild.style.visibility = 'visible';
+    });
+
+  });
+});
